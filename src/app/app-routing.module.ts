73691a4f288d8164d2a4156232f4bec48428c80a
Ki Jung Kim <kijungkim@eworldes.com>
2020-01-06 10:27:15 -1000

    added home and catchall routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'page-not-found', component: NotFoundComponent },
+  { path: '**', redirectTo: '/page-not-found' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
